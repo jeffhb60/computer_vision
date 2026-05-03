c8535c4dce204bdb1dd20797ec6f3caee2264dd3
Jeff Butt <jbutt2@wgu.edu>
2026-05-02 21:24:05 -0400

    Add Fashion-MNIST label mappings

---
+++ app/labels.py
@@ -0,0 +1,26 @@
+FASHION_MNIST_LABELS = {
+    0: "T-shirt/top",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle boot",
+}
+
+
+SAFE_LABEL_NAMES = {
+    0: "tshirt_top",
+    1: "trouser",
+    2: "pullover",
+    3: "dress",
+    4: "coat",
+    5: "sandal",
+    6: "shirt",
+    7: "sneaker",
+    8: "bag",
+    9: "ankle_boot",
+}
